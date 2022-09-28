cea9e4c24c4535031aa739c4502c39572b479c31
Joshua Wootonn <josh@makeswift.com>
2022-09-28 16:16:14 -0500

    Revert "chore: upgrade @makeswift/runtime"
    This reverts commit 82b50b42c64230dda3b0fb5223d94075e7bdd543.

--- pages/api/makeswift/[...makeswift].ts
+++
@@ -1,7 +0,0 @@
-import { MakeswiftApiHandler } from '@makeswift/runtime/next'
-
-import { getConfig } from 'lib/config'
-
-const config = getConfig()
-
-export default MakeswiftApiHandler(config.makeswiftSiteApiKey)
