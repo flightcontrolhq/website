d04c4f429abec03c379eef35c4ea6f2b40ab0516
Joshua Wootonn <josh@makeswift.com>
2022-10-11 09:59:50 -0500

    fix: tweak plane initial animation for iphone bug

--- components/Plane/Plane.tsx
+++ components/Plane/Plane.tsx
@@ -1,6 +1,6 @@
-import { motion, useScroll, useSpring, useTransform } from 'framer-motion'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import Image from 'next/image'
-import { forwardRef, Ref, useEffect, useImperativeHandle, useRef, useState } from 'react'
+import { forwardRef, Ref, useImperativeHandle, useLayoutEffect, useRef, useState } from 'react'
 
 type Props = {
   isAboveTheFold?: boolean
@@ -17,7 +17,7 @@ export const Plane = forwardRef(function Plane(
 
   const [width, setWidth] = useState(0)
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     window.addEventListener('resize', resize)
     resize()
     return () => window.removeEventListener('resize', resize)
@@ -30,10 +30,11 @@ export const Plane = forwardRef(function Plane(
 
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ['0 1.5', '1 -.2'],
+    offset: ['0 1.1', '1 -.1'],
   })
   const x = useTransform(scrollYProgress, [0, 1], [0, width])
-  const smoothX = useSpring(x, { duration: 0.5 })
+  const isWidthSet = width > 0
+
   return (
     <motion.div
       initial={{
@@ -42,16 +43,16 @@ export const Plane = forwardRef(function Plane(
         x: '-70%',
       }}
       animate={{
-        opacity: isLoaded ? 1 : 0,
+        opacity: isLoaded && isWidthSet ? 1 : 0,
         y: '-50%',
-        x: isLoaded ? '-50%' : '-70%',
+        x: isLoaded && isWidthSet ? '-50%' : '-70%',
       }}
       transition={{ duration: 0.3, type: 'tween', ease: [0.165, 0.84, 0.44, 1] }}
       className={`absolute w-full z-[-1] top-1/2 left-1/2`}
       ref={ref}
       {...props}
     >
-      <motion.div style={{ x: smoothX }}>
+      <motion.div style={{ x }}>
         <div className="transform -translate-x-1/2 w-[400px] tablet:w-[500px] desktop:w-[600px]">
           <Image
             onLoadingComplete={() => {
