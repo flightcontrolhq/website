b86f2b8603755ae189a98b1206e668ea5cb48aae
Joshua Wootonn <josh@makeswift.com>
2022-10-13 16:49:53 -0500

    feat: add sanity studio

---
+++ studio/schemas/blog-post.js
@@ -0,0 +1,45 @@
+export default {
+  name: 'blog-post',
+  type: 'document',
+  title: 'Blog Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: { type: 'author' },
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    },
+
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'tag' } }],
+    },
+    {
+      name: 'content',
+      type: 'array',
+      title: 'Content',
+      of: [{ type: 'block' }, { type: 'code' }],
+    },
+  ],
+}
