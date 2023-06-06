3d6fa8149fd54276b6ea4f0b937b3ab586b204f4
Brandon Bayer <b@bayer.ws>
2023-06-06 14:54:25 -0400

    remove motif docs service & code

--- motif-docs/lib/files.ts
+++
@@ -1,3 +0,0 @@
-export const removeFileExtension = (name: string) => {
-  return name.replace(/.(md|mdx|mdoc)$/, "")
-}
--- motif-docs/lib/types.ts
+++
@@ -1,7 +0,0 @@
-export type SidebarContent = SidebarSection[];
-
-export interface SidebarSection {
-  title: string;
-  href?: string;
-  pages?: SidebarSection[];
-}
