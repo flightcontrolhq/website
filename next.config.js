7c246dda8579e0b0bca1bee2f7fe3c79ee820949
Brandon Bayer <b@bayer.ws>
2023-06-06 15:00:19 -0400

    remove DOCS_URL

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const withBlitz = require('@blitzjs/next').withBlitz
 const withMakeswiftInitializer = require('@makeswift/runtime/next/plugin')
 
 const withMakeswift = withMakeswiftInitializer()
-const { DOCS_URL } = process.env
 
-console.log('DOCS_URL', DOCS_URL)
 module.exports = withBlitz(
   withMakeswift({
     reactStrictMode: true,
@@ -14,20 +12,11 @@ module.exports = withBlitz(
       domains: ['s.mkswft.com', 'cdn.sanity.io', '*.motif.land', 'res.cloudinary.com'],
     },
     async rewrites() {
-      invariant(DOCS_URL, 'DOCS_URL env var is not defined.')
       return {
         beforeFiles: [
           {
             source: '/home',
             destination: `/`,
-          },
-          {
-            source: '/docs',
-            destination: `${DOCS_URL}/docs`,
-          },
-          {
-            source: '/docs/:path*',
-            destination: `${DOCS_URL}/docs/:path*`,
           },
         ],
       }
